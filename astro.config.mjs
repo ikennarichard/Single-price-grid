77b991b7b96c2088c014e220397c84b507a088de
Ikenna Richard <oguejioforrichard@gmail.com>
2023-01-12 00:43:21 -1100

    Update configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://single-price.github.io',
-    // base: '/Single-price-grid'
+    site: 'https://ikennarichard.github.io',
+    base: '/Single-price-grid'
 })
